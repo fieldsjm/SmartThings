e5b7ca6bdbe3ef408e964d755b686ccc90d2d05f
Jonathan Fields <liljonnyfields@gmail.com>
2019-08-21 11:30:55 -0400

    addition - dock momentary tile

--- devicetypes/smartthings/roomba-control.src/roomba-control.groovy
+++ devicetypes/smartthings/roomba-control.src/roomba-control.groovy
@@ -3,13 +3,16 @@ metadata {
 		capability "Actuator"
 		capability "Door Control"
         capability "Garage Door Control"
+        capability "Momentary"
 		capability "Refresh"
 		capability "Sensor"
+        capability "Switch"
         
         command "start"
         command "pause"
         command "running"
         command "complete"
+        command "dock"
 	}
 
 	simulator {
@@ -18,10 +21,10 @@ metadata {
 
 	tiles {
 		standardTile("toggle", "device.door", inactiveLabel: true, width: 3, height: 3) {
-			state("closed", label:"Start", action:"door control.open", icon:"st.Electronics.electronics13", backgroundColor:"#8CFC03")
+			state("closed", label:"Start", action:"door control.open", icon:"st.Electronics.electronics13", backgroundColor:"#8CFC03", nextState:"opening")
 			state("opening", label:"In-Process", icon:"st.Health & Wellness.health7", backgroundColor:"#078bf7")
 			state("open", label:"Paused", action:"door control.close", icon:"st.sonos.pause-btn", backgroundColor:"#FC030F")
-			state("closing", label:"Complete", icon:"st.nest.nest-leaf", backgroundColor:"#5F07F7")
+			state("closing", label:"Complete", icon:"st.nest.nest-leaf", backgroundColor:"#5F07F7", nextState:"closed")
 			
 		}
 		standardTile("open", "device.door", inactiveLabel: false, decoration: "flat") {
@@ -38,10 +41,14 @@ metadata {
 		}
         standardTile("refresh", "device.alarmStatus", inactiveLabel: false, decoration: "flat") {
         	state "refresh", action:"polling.poll", icon:"st.secondary.refresh"
-            }
+        }
+        standardTile("switch", "device.switch", inactiveLabel: false, decoration: "flat"){
+			state("off", label: "Dock", icon: "st.presence.house.unlocked", action: "momentary.push", backgroundColor: "#ffffff", nextState: "on", defaultState: "true")
+			state("on", label: "", action: "momentary.push", backgroundColor: "#00a0dc")
+		}
 
 		main "toggle"
-		details(["toggle", "open", "close", "refresh"])
+		details(["toggle", "open", "close", "switch", "refresh"])
 	}
 }
 
@@ -63,4 +70,18 @@ def running() {
 
 def complete() {
     sendEvent(name: "door", value: "closed")
+}
+
+def push() {
+	sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
+	sendEvent(name: "switch", value: "off", isStateChange: true, displayed: false)
+	sendEvent(name: "momentary", value: "pushed", isStateChange: true)
+}
+
+def on() {
+	push()
+}
+
+def off() {
+	push()
 }
