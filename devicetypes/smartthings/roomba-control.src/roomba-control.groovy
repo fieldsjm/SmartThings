bde66b96c1dccdfa8f816dcd01b298a431c398c7
Jonathan Fields <liljonnyfields@gmail.com>
2019-08-23 14:02:07 -0400

    update

--- devicetypes/smartthings/roomba-control.src/roomba-control.groovy
+++ devicetypes/smartthings/roomba-control.src/roomba-control.groovy
@@ -21,67 +21,70 @@ metadata {
 
 	tiles {
 		standardTile("toggle", "device.door", inactiveLabel: true, width: 3, height: 3) {
-			state("closed", label:"Start", action:"door control.open", icon:"st.Electronics.electronics13", backgroundColor:"#8CFC03", nextState:"opening")
+			state("closed", label:"Start", action:"door control.open", icon:"st.Electronics.electronics13", backgroundColor:"#8CFC03")
 			state("opening", label:"In-Process", icon:"st.Health & Wellness.health7", backgroundColor:"#078bf7")
-			state("open", label:"Paused", action:"door control.close", icon:"st.sonos.pause-btn", backgroundColor:"#FC030F")
-			state("closing", label:"Complete", icon:"st.nest.nest-leaf", backgroundColor:"#5F07F7", nextState:"closed")
+			state("closing", label:"Paused", action:"door control.close", icon:"st.sonos.pause-btn", backgroundColor:"#FC030F")
+			state("open", label:"Dock", icon:"st.presence.house.unlocked", backgroundColor:"#5F07F7")
 			
 		}
-		standardTile("open", "device.door", inactiveLabel: false, decoration: "flat") {
+		standardTile("start", "device.door", inactiveLabel: false, decoration: "flat") {
 			state "default", label:"Start", action:"door control.open", icon:"st.Electronics.electronics13"
 		}
-		standardTile("close", "device.door", inactiveLabel: false, decoration: "flat") {
+		standardTile("pause", "device.door", inactiveLabel: false, decoration: "flat") {
 			state "default", label:"", action:"door control.close", icon:"st.sonos.pause-btn"
 		}
-        standardTile("running", "device.door", inactiveLabel: false, decoration: "flat") {
-			state "default", label:"Running", action:"running", icon:"st.Health & Wellness.health7"
-		}
-        standardTile("complete", "device.door", inactiveLabel: false, decoration: "flat") {
-			state "default", label:"Complete", action:"complete", icon:"st.nest.nest-leaf"
-		}
-        standardTile("refresh", "device.alarmStatus", inactiveLabel: false, decoration: "flat") {
-        	state "refresh", action:"polling.poll", icon:"st.secondary.refresh"
-        }
-        standardTile("switch", "device.switch", inactiveLabel: false, decoration: "flat"){
-			state("off", label: "Dock", icon: "st.presence.house.unlocked", action: "momentary.push", backgroundColor: "#ffffff", nextState: "on", defaultState: "true")
-			state("on", label: "", action: "momentary.push", backgroundColor: "#00a0dc")
+        standardTile("dock", "device.door", inactiveLabel: false, decoration: "flat") {
+			state "default", label:"Dock", action:"dock", icon:"st.presence.house.unlocked"
 		}
 
-		main "toggle"
-		details(["toggle", "open", "close", "switch", "refresh"])
+		standardTile("switch", "device.switch", inactiveLabel: false, width: 3, height: 1){
+			state("off", label: "", icon: "st.nest.empty", action: "switch.on", backgroundColor: "#ffffff", nextState: "on", defaultState: "true")
+			state("on", label: "Complete", action: "switch.off", backgroundColor: "#00a0dc")
+		}
+		valueTile("Status", "device.longText", width: 3, height: 2) {
+			state ("Ready", label: "Ready", icon:"st.Electronics.electronics13", backgroundColor:"#8CFC03")
+            state ("Running", label: "Running", icon:"st.Health & Wellness.health7", backgroundColor:"#078bf7")
+            state ("Paused", label: "Paused", icon:"st.sonos.pause-btn", backgroundColor:"#FC030F")
+            state ("Docking", label: "Docking", icon:"st.presence.house.unlocked", backgroundColor:"#5F07F7")
+            state ("Finished", label: "Finished", icon:"st.Electronics.electronics13", backgroundColor:"#8CFC03")
+            
+		}
+
+		main "Status"
+		details(["Status", "start", "pause", "dock"])
 	}
 }
 
 def parse(String description) {
 	log.trace "parse($description)"
 }
+def installed() {
+	sendEvent(name: "longText", value: "Ready")
+    sendEvent(name: "switch", value: "off")
+    
+    }
 
-def open() {
+def start() {
 	sendEvent(name: "door", value: "opening")
+    sendEvent(name: "longText", value: "Running")
 }
 
-def close() {
+def pause() {
     sendEvent(name: "door", value: "closing")
+    sendEvent(name: "longText", value: "Paused")
 }
 
-def running() {
-    sendEvent(name: "door", value: "open")
-}
-
-def complete() {
+def dock() {
     sendEvent(name: "door", value: "closed")
-}
-
-def push() {
-	sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
-	sendEvent(name: "switch", value: "off", isStateChange: true, displayed: false)
-	sendEvent(name: "momentary", value: "pushed", isStateChange: true)
+    sendEvent(name: "longText", value: "Docking")
 }
 
 def on() {
-	push()
+    sendEvent(name: "switch", value: "on")
+    sendEvent(name: "longText", value: "Finished")
 }
 
 def off() {
-	push()
+	sendEvent(name: "switch", value: "off")
+    sendEvent(name: "longText", value: "Ready")
 }
